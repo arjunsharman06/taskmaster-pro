76d2a80defd7f9d68d093a9966c09045242e94bc
ARJUN <arjunsharman06@gmail.com>
2022-05-31 11:43:38 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,7 +168,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -204,16 +204,20 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(event.target).addClass('dropover-active');
+    console.log(event.target);
+    console.log($(event.target));
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(event.target).removeClass('dropover-active');
   },
 
   update: function (event) {
@@ -236,7 +240,7 @@ $(".card .list-group").sortable({
         date: date
       });
 
-      console.log(text, date);
+      // console.log(text, date);
     });
 
     // trim down list's ID to match object property
@@ -255,14 +259,14 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function(event, ui) {
-    console.log("drop");
     ui.draggable.remove();
+    $('.bottom-trash').removeClass('bottom-trash-active');
   },
   over: function(event, ui) {
-    console.log("over");
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function(event, ui) {
-    console.log("out");
+    $('.bottom-trash').removeClass('bottom-trash-active');
   }
 });
 
@@ -274,7 +278,7 @@ var auditTask = function(taskEl) {
   // get date from the task element
   var date = $(taskEl).find('span').text().trim();
 
-  console.log(date);
+  // console.log(date);
   
    // convert to moment object at 5:00pm
    var time = moment(date,"L").set("hour", 17);
@@ -289,7 +293,15 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 
+
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, ((1000 * 60) * 30));
+
 // load tasks for the first time
 loadTasks();
 
